09f75131ed2156a84af89a685629c55eade0b236
Hiago Prates <hiagohps360@gmail.com>
2024-06-12 10:05:00 -0300

    feat: config status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,27 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import 'react-native-reanimated'
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <Text>Hello world!</Text> : <></>}
     </View>
   )
