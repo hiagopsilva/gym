036e19cbb001f18c2efc4a0fa21bd65041700822
Hiago Prates <hiagohps360@gmail.com>
2024-06-12 10:00:57 -0300

    feat: config eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
-import 'react-native-reanimated';
-
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { Text, View } from 'react-native'
+import 'react-native-reanimated'
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1}} >
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
       {fontsLoaded ? <Text>Hello world!</Text> : <></>}
     </View>
-  );
+  )
 }
